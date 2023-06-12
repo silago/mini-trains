b97504791d8916242d183a6a86118c04af89fac7
silago <silago.nevermind@gmail.com>
2023-06-12 22:41:56 +0300

    initial rail switcher

---
+++ Assets/Scripts/RailSwitcher.cs
@@ -0,0 +1,9 @@
+// Copyright (C) 2023 White Sharx (https://whitesharx.com) - All Rights Reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+
+using UnityEngine;
+
+public class RailSwitcher : MonoBehaviour{
+  
+}
